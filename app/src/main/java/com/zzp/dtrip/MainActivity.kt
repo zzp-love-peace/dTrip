7bc3c0f139bf96f2494b5fe17935ed061e074f00
zzploveandpace <80821115+zzploveandpace@users.noreply.github.com>
2021-08-05 16:42:21 +0800

    zzp

---
+++ app/src/main/java/com/zzp/dtrip/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.zzp.dtrip
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.zzp.dtrip.fragment.MineFragment
+import com.zzp.dtrip.fragment.TripFragment
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var tripFragment: TripFragment
+    private lateinit var mineFragment: MineFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        tripFragment = TripFragment()
+        mineFragment = MineFragment()
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.add(R.id.nav_host_fragment, tripFragment)
+        transaction.add(R.id.nav_host_fragment, mineFragment)
+        transaction.hide(mineFragment)
+        transaction.commit()
+        navView.setOnNavigationItemSelectedListener {
+            it.isChecked = true
+            val transaction2 = fragmentManager.beginTransaction()
+            when (it.itemId) {
+                R.id.navigation_trip -> {
+                    transaction2.hide(mineFragment)
+                    transaction2.show(tripFragment)
+                }
+                R.id.navigation_mine -> {
+                    transaction2.hide(tripFragment)
+                    transaction2.show(mineFragment)
+                }
+            }
+            transaction2.commit()
+            false
+        }
+    }
+}
---
+++ app/src/main/java/com/zzp/dtrip/fragment/MineFragment.kt
@@ -0,0 +1,18 @@
+package com.zzp.dtrip.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.zzp.dtrip.R
+
+class MineFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+        val root: View = inflater.inflate(R.layout.fragment_mine, container, false)
+        return root
+    }
+}
